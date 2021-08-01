1ca65cff56602e26e220e6c07ee733a193468981
karthiksajeev94 <31260021+karthiksajeev94@users.noreply.github.com>
2021-08-01 07:30:54 -0500

    Added Simple Algos

---
+++ SimpleAlgos/src/grokkingAlgos/HCF.java
@@ -0,0 +1,19 @@
+package grokkingAlgos;
+
+public class HCF {
+	
+	int x;
+	int y;
+
+	public static void main(String[] args) {
+		int hcf;
+//		hcf=getHCF(5,20);
+//		System.out.println(getHCF(5,20));
+		System.out.println("Hello World");
+	}
+	
+	public int getHCF(int x, int y) {
+		return 0;
+	}
+
+}
